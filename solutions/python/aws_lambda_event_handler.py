45f557dd5ed39cdae77719f1737ab071b23fdfda
andylongshaw <andy.longshaw@coopdigital.co.uk>
2017-06-21 21:36:27 +0100

    working version of event lambda with example record format

--- solutions/python/aws_lambda_event_handler.py
+++ solutions/python/aws_lambda_event_handler.py
@@ -1,19 +1,22 @@
 import boto3
 import json
+import uuid
 
-print('Loading function')
+# print('Loading function')
 dynamo = boto3.resource("dynamodb", region_name='eu-west-1')
-table_name = 'ZebraPendingEmails'
+table_name = 'Bat-PendingEmailTable'
 
 
 def lambda_handler(event, context):
-    # print("Received event: " + json.dumps(event, indent=2))
-    for record in event['Records']:
-        create_email_record(record('eventID'), record['name'])
+    print("Received event: " + json.dumps(event, indent=2))
+    for recordInfo in event['Records']:
+        record = recordInfo['dynamodb']['NewImage']
+        create_email_record(record['customerId']['S'], record['name']['S'])
 
 
-def create_email_record(email_id, customer_name):
+def create_email_record(customer_id, customer_name):
     table = dynamo.Table(table_name)
+    email_id = "{}_{}".format(customer_id, str(uuid.uuid4()))
     response = table.put_item(
         Item={
             'emailId': email_id,
